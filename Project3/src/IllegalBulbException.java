5416c5aad98b789d6f77e809fb09c44d8f30fa68
amybutler880 <70174691+amybutler880@users.noreply.github.com>
2021-04-25 21:49:37 -0400

    Delete Project3 directory

--- Project3/src/IllegalBulbException.java
+++
@@ -1,6 +0,0 @@
-public class IllegalBulbException extends IllegalArgumentException  {
-	 public IllegalBulbException(String message) {
-		 super(message); //call the super class constructor
-	 }
-}
-
