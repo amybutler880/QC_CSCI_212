2d54110364d11fcea495dfc748493e388ad94720
amybutler880 <70174691+amybutler880@users.noreply.github.com>
2021-05-10 21:05:11 -0400

    Delete BulbComparator.java

--- BulbComparator.java
+++
@@ -1,7 +0,0 @@
-import java.util.Comparator;
-
-public class BulbComparator implements Comparator <Bulb>  { //Bulb comparator
-	   public int compare(Bulb num1, Bulb num2) { //Compare two bulb part numbers
-		      return num1.compareTo(num2); //Return one of the bulbs - calls on compareTo method
-	   }
-}
